cc91bc8f0b19d9cb17a96b72e38bd227ad447a31
vgrazi <vgrazi@gmail.com>
2022-11-22 21:20:34 -0500

    Added logging to jframe

--- src/main/java/com/example/keepalive/KeepaliveApplication.java
+++ src/main/java/com/example/keepalive/KeepaliveApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Date;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,50 +17,76 @@ import java.util.concurrent.Executors;
 @SpringBootApplication
 public class KeepaliveApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        SpringApplicationBuilder builder = new SpringApplicationBuilder(KeepaliveApplication.class);
-        builder.headless(false);
-        ConfigurableApplicationContext context = builder.run(args);
-    }
+    private static final int SLEEP_TIME = 120 * 10;
+    private final JFrame frame=new JFrame("Keepalive");
+    private final JTextArea textArea=new JTextArea();
+    private final JScrollPane scrollPane=new JScrollPane(textArea);
+    private int position;
 
-    private final JFrame frame = new JFrame("Keepalive");
+    public static void main(String[] args) {
+        SpringApplicationBuilder builder=new SpringApplicationBuilder(KeepaliveApplication.class);
+        builder.headless(false);
+        ConfigurableApplicationContext context=builder.run(args);
+    }
 
     @Override
     public void run(String... args) throws Exception {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JButton button = new JButton();
-        button.addActionListener((event)->{
-            Point location = MouseInfo.getPointerInfo().getLocation();
+        JButton button=new JButton();
+        button.addActionListener((event) -> {
+            textArea.setText("");
+            Point location=MouseInfo.getPointerInfo().getLocation();
+            message(location);
 //            log.info(location.toString());
         });
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        frame.getContentPane().add(button);
-        Point startLocation = MouseInfo.getPointerInfo().getLocation();
-        frame.setBounds(startLocation.x-50, startLocation.y-10, 500,60);
+        ExecutorService executor=Executors.newSingleThreadExecutor();
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.getContentPane().add(BorderLayout.CENTER, scrollPane);
+        Point startLocation=MouseInfo.getPointerInfo().getLocation();
+        frame.setBounds(startLocation.x - 50, startLocation.y - 10, 500, 300);
         frame.setVisible(true);
-        Robot r = new Robot();
-        CompletableFuture.runAsync(()->{
-            Point lastLocation = MouseInfo.getPointerInfo().getLocation();
-            int direction = 1;
-            while (true){
-                // check the last mouse position
-                Point location = MouseInfo.getPointerInfo().getLocation();
-                // if we have moved since the last check, do nothing
-                if (location.equals(lastLocation)) {// otherwise, move it one pixel
-                    // set the new value for the last mouse position
-                    lastLocation = new Point(location.x + direction, location.y);
-                    moveTo(r, location.x + direction, location.y);
+        Robot r=new Robot();
+        CompletableFuture.runAsync(() -> {
+            Point lastLocation=MouseInfo.getPointerInfo().getLocation();
+            int direction=1;
+            try {
+                while(true) {
+                    // check the last mouse position
+                    Point location=MouseInfo.getPointerInfo().getLocation();
+                    // if we have moved since the last check, do nothing
+                    if(location.equals(lastLocation)) {// otherwise, move it one pixel
+                        // set the new value for the last mouse position
+                        lastLocation=new Point(location.x + direction, location.y);
+                        moveTo(r, location.x + direction, location.y);
+                        message(MouseInfo.getPointerInfo().getLocation());
 
-                    // toggle direction
-                    direction = -direction;
-                } else {
-//                    log.info(String.format("Current location:%s; Last location: %s. Skipping", location, lastLocation));
-                    lastLocation = location;
+                        // toggle direction
+                        direction=-direction;
+                    } else {
+                        //                    log.info(String.format("Current location:%s; Last location: %s. Skipping", location, lastLocation));
+                        lastLocation=location;
+                    }
+                    sleep(SLEEP_TIME);
                 }
-                sleep(60*2000);
+            } catch(Error e) {
+                e.printStackTrace();
+                message(e);
+            } finally {
+                message("Exiting");
             }
 
+
         }, executor);
+    }
+
+    private void message(Object message) {
+        textArea.append(new Date().toString());
+        textArea.append(" ");
+        textArea.append(String.valueOf(++position));
+        textArea.append(" ");
+        textArea.append(String.valueOf(message));
+        textArea.append("\r\n");
+        textArea.setCaretPosition(textArea.getText().length());
     }
 
     private void moveTo(Robot r, int x, int y) {
@@ -72,7 +99,7 @@ public class KeepaliveApplication implements CommandLineRunner {
     private void sleep(long time) {
         try {
             Thread.sleep(time);
-        } catch (InterruptedException e) {
+        } catch(InterruptedException e) {
             Thread.currentThread().interrupt();
         }
     }
